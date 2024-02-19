b0c9d597110446b5087d25dac0a5af5665cb948e
K1rZa <kirill.white7335@gmail.com>
2024-02-19 15:32:23 +0400

    modified:   game-code.js

--- game-code.js
+++ game-code.js
@@ -14,8 +14,8 @@ var speed = 0.5
 var score = 0
 
 let food = {
-	x: food_position_x(),
-	y: food_position_y(),
+	x: food_position().x,
+	y: food_position().y,
 }
 
 let snake = []
@@ -27,14 +27,21 @@ snake[0] = {
 function random_int(min, max) {
 	return Math.floor(Math.random() * (max - (min - 1))) + min
 }
-function food_position_x() {
+/*function food_position_x() {
 	let x = random_int(1, width_area - 2) * tile
- /*let y = random_int(1, height_area - 2) * tile*/
 	return x
 }
 function food_position_y() {
 	let y = random_int(1, height_area - 2) * tile
 	return y
+}*/
+function food_position(){
+	let x = random_int(1, width_area - 2) * tile
+	let y = random_int(1, height_area - 2) * tile
+	return {
+		x: x,
+		y: y,
+	}
 }
 function drawgame() {
 	context.drawImage(ground_img, 0, 0)
